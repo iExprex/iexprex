a00b6ba513da508c76cff986e7827ee727a2f4e4
iExprex <iexprexghana@gmail.com>
2024-07-31 23:46:39 +0000

    Added firebase hosting

--- src/widget/Services.jsx
+++ src/widget/Services.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import bgImg from '../assets/blog77.jpg';
 const Services = () => {
   return (
     <ServicesContainerWrapper>
-
+      <ServicesHederContainerWrapper></ServicesHederContainerWrapper>
     </ServicesContainerWrapper>
   )
 }
@@ -14,6 +14,13 @@ const ServicesContainerWrapper = styled.section`
     height: 100%;
     background: rgba(137, 43, 226, 0.283);
     border-radius: inherit;
+`
+const ServicesHederContainerWrapper = styled.header`
+ width: 100%;
+ height: 100vh;
+ background-image: url(${bgImg});
+ background-position: center;
+ background-size: cover;
 `
 
 export default Services
